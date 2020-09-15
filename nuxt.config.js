4729dcedac38c05effe9fd4fb60a0ca512f65059
UM682 <skn.fjc.bbb@gmail.com>
2020-09-15 11:28:04 +0900

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+const title = 'sketches'
+const description = 'sketches'
+const url = 'https://sharp-mcnulty-a52ff3.netlify.app/'
+const ogImage = `https://lh3.googleusercontent.com/hOjUfZCv3t0aJqcjG5JH98VN_64QlnHUbycNC1c08UIRGMPHkj-utp8eVZfcmCpqQyXQG-KYavIo_FRra4xU6GoK-3NHLcBEQ1nvC_AXTmqL4Wii4bsv5oJQoI5X5Hh6E9yx_viaiUk=w800-h450`
 
 export default {
   /*
@@ -15,18 +19,46 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'sketches',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: description },
+      { hid: 'ogTitle', property: 'og:title', content: title },
+      { hid: 'ogType', property: 'og:type', content: 'website' },
+      { hid: 'ogUrl', property: 'og:url', content: url },
+      {
+        hid: 'ogImage',
+        property: 'og:image',
+        content: ogImage
+      },
+      { property: 'og:site_name', content: title },
+      {
+        hid: 'ogDescription',
+        property: 'og:description',
+        content: description
+      },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitterSite', name: 'twitter:site', content: title },
+      { hid: 'twitterUrl', name: 'twitter:url', content: url },
+      { hid: 'twitterTitle', name: 'twitter:title', content: title },
+      {
+        hid: 'twitterDescription',
+        name: 'twitter:description',
+        content: description
+      },
+      {
+        hid: 'twitterImage',
+        name: 'twitter:image:src',
+        content: ogImage
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap'
       },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } // ここを変更する
     ]
   },
   /*

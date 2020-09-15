581d7d430786eddcba8682f67b70f1f53c61e4d3
UM682 <skn.fjc.bbb@gmail.com>
2020-09-15 12:09:31 +0900

    commit

---
+++ components/Artwork0915/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="artwork">
+    <div id="canvas-container2"></div>
+  </section>
+</template>
+
+<script>
+import Artwork0915 from "./js/Artwork0915";
+
+export default {
+  name: "artwork0915",
+  components: {},
+  props: [],
+  data() {
+    // 基本的にはここにthree.jsのオブジェクトを追加しない。
+    return {};
+  },
+  computed: {},
+  mounted() {
+    // canvas要素を渡す。
+    this.Artwork0915 = new Artwork0915({
+      $canvas: this.$refs.canvas,
+    });
+  },
+  destroyed() {
+    // canvasを作ったり壊したりする前提の場合はここに処理停止する処理を書く（今回省略）。
+  },
+  methods: {
+    // この中にthree.jsの処理をばりばり書かない。
+  },
+};
+</script>
+
+<style>
+/* スタイルなどお好みで　*/
+.artwork {
+  position: fixed;
+}
+</style>
--- components/Navlink.vue
+++ components/Navlink.vue
@@ -1,17 +1,32 @@
 <template>
   <nav class="ph3 ph5-ns" style="background-color:#333333;">
     <div class="nowrap">
+      <a class="link dim moon-gray { color: #CCCCCC } f5 f4-ns dib mr3" href="/" title="09/01">09/01</a>
       <a
         class="link dim moon-gray { color: #CCCCCC } f5 f4-ns dib mr3"
-        href="/"
-        title="Link 1"
-      >09/01</a>
-      <a
-        class="link dim moon-gray { color: #CCCCCC } f5 f4-ns dib mr3"
-        href="0914"
-        title="Link 2"
+        href="/0914"
+        title="09/14"
       >09/14</a>
+      <a
+        class="link dim moon-gray { color: #CCCCCC } f5 f4-ns dib mr3"
+        href="/0915"
+        title="09/15"
+      >09/15</a>
     </div>
   </nav>
 </template>
+<style>
+html {
+  font-family: "Noto Sans JP", sans-serif, -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 14px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
 
+  background-color: #333333;
+}
+</style>
